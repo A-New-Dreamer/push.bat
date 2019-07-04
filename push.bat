31a4f92ecc680e33ce3719656af94c144617301f
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-07-04 10:55:54 +0800

    update at 10:55:53.94

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 git add .
 set /p UserCommit="提交理由:"
 if "%UserCommit%" == "" (
-    git commit -m "update at%time%"
+    git commit -m "update at %time%"
 ) else (
     git commit -m %UserCommit%
 )
